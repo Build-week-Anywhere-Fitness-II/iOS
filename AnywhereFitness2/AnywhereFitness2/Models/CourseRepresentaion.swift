0110e07e93ae1a53387f5dfd016e85eb2309df57
Zachary Thacker <zachthac@aol.com>
2020-10-16 00:10:50 -0400

    set up core data

---
+++ AnywhereFitness2/AnywhereFitness2/Models/CourseRepresentaion.swift
@@ -0,0 +1,12 @@
+//
+//  CourseRepresentaion.swift
+//  AnywhereFitness2
+//
+//  Created by Zachary Thacker on 10/15/20.
+//  Copyright © 2020 John McCants. All rights reserved.
+//
+
+import Foundation
+
+
+
